8f60c55df947b1f77893358211fafbca5eed24b7
Seoungwoo Hong <hongseou@msu.edu>
2022-06-20 01:37:00 -0400

    push23

--- public/cart.js
+++ public/cart.js
@@ -1,5 +1,5 @@
 $(function() {
     $('#cart-form').on('submit', function() {
-        alert("Yor order request has been submitted. Please wait until the request is approved.");
+        alert("Your order request has been submitted. Please wait until the request is approved.");
     });
 });
